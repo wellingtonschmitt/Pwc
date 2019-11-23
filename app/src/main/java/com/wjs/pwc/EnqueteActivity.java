ddb2838de5a8a2a8d99ed3db92c17c8d177823f5
Wellington <wellington.schmitt@gmail.com>
2019-11-23 15:28:28 -0300

    PwC teste Abacomm -base app -activity de login

---
+++ app/src/main/java/com/wjs/pwc/EnqueteActivity.java
@@ -0,0 +1,14 @@
+package com.wjs.pwc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnqueteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enquete);
+    }
+}
