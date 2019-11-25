4ba67910758d05475cc487dd71f3957a2fcc3c12
Wellington <wellington.schmitt@gmail.com>
2019-11-24 22:02:02 -0300

    add: -fix layout login -fix fake auth remove check permissions

--- app/src/main/java/com/wjs/pwc/LoginActivity.java
+++ app/src/main/java/com/wjs/pwc/LoginActivity.java
@@ -4,11 +4,20 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.SpannableStringBuilder;
+import android.text.TextWatcher;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -31,14 +40,45 @@ public class LoginActivity extends AppCompatActivity {
 
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        final EditText username = findViewById(R.id.username);
+        final EditText password = findViewById(R.id.password);
+        final TextView registreSe = findViewById(R.id.registreSe);
 
-        loginButton.setOnClickListener(new LoginClickListener(this,
-                (EditText) findViewById(R.id.username),
-                (EditText) findViewById(R.id.password)));
+        String strCad = getString(R.string.ainda_nao_tem_cadastro);
+        String strReg = getString(R.string.registre_se);
+        strCad = strCad + " " + strReg;
+        SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder(strCad);
+        spannableStringBuilder.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.pumpkin_orange)),
+                strCad.length() - strReg.length() - 1, strCad.length(), 18);
+        spannableStringBuilder.setSpan(new RelativeSizeSpan(1.0F), strCad.length() - strReg.length() - 1, strCad.length(), 33);
+        registreSe.setText(spannableStringBuilder);
+        registreSe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNotToday();
+            }
+        });
+        findViewById(R.id.esqueceuSenha).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNotToday();
+            }
+        });
 
-        requestPermission();
+        username.addTextChangedListener(new LoginDataTextWatcher(loginButton,username,password));
+        password.addTextChangedListener(new LoginDataTextWatcher(loginButton,username,password));
+
+        loginButton.setOnClickListener(new LoginClickListener(this,username,password));
+        password.setOnEditorActionListener(new PasswordActionListener(loginButton));
+        //TODO
+        //requestPermission();
     }
 
+    void showNotToday(){
+        Toast.makeText(LoginActivity.this,R.string.not_today,Toast.LENGTH_SHORT).show();
+    }
+
+    //region [perms]
     private void requestPermission() {
         List<String> permissionList = new ArrayList<>(3);
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
@@ -79,12 +119,65 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
     }
+    //endregion
+
+    private class PasswordActionListener implements TextView.OnEditorActionListener {
+        private WeakReference<Button> login;
+
+        public PasswordActionListener(Button login) {
+            this.login = new WeakReference<>(login);
+        }
+
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            final Button loginBtn = login.get();
+            if (loginBtn.isEnabled()) {
+                loginBtn.performClick();
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private class LoginDataTextWatcher implements TextWatcher{
+
+        private WeakReference<EditText> username;
+        private WeakReference<EditText> password;
+        private WeakReference<Button> login;
+
+        public LoginDataTextWatcher(final Button login, final EditText username, final EditText password) {
+            this.login = new WeakReference<>(login);
+            this.username = new WeakReference<>(username);
+            this.password = new WeakReference<>(password);
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            final EditText usernameEditText = username.get();
+            final EditText passwordEditText = password.get();
+            final Button loginBtn = login.get();
+            if (loginBtn == null || usernameEditText == null || passwordEditText == null)
+                return;
+            loginBtn.setEnabled(usernameEditText.getText().toString().trim().length() > 0 &&
+                    passwordEditText.getText().toString().trim().length() > 0);
+        }
+    }
 
     private class LoginClickListener implements View.OnClickListener {
 
-        WeakReference<Activity> activity;
-        WeakReference<EditText> username;
-        WeakReference<EditText> password;
+        private WeakReference<Activity> activity;
+        private WeakReference<EditText> username;
+        private WeakReference<EditText> password;
 
         public LoginClickListener(final Activity activity, final EditText username, final EditText password) {
             this.activity = new WeakReference<>(activity);
@@ -106,7 +199,7 @@ public class LoginActivity extends AppCompatActivity {
             }
             if (!pass.equals(passwordEditText.getText().toString().trim())) {
                 hasError = true;
-                usernameEditText.setError(getString(R.string.senha_invalido));
+                passwordEditText.setError(getString(R.string.senha_invalido));
             }
             if (!hasError) {
                 setResult(Activity.RESULT_OK);
